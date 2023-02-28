96758932d6bd2ae063939c0f6eef134299072f21
gijsvandenbeuken <g.vandenbeuken@student.fontys.nl>
2023-02-28 14:55:00 +0100

    test voorbeeld

--- Engineering-ASPNET/Program.cs
+++ Engineering-ASPNET/Program.cs
@@ -15,7 +15,8 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    
+
+    string x = "x";
 }
 
 app.UseHttpsRedirection();
