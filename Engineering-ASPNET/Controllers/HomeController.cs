4d5000f9a94a18a138db6075469feb953a84127c
Zamo046 <Zamo_nizar@hotmail.com>
2023-03-02 09:16:33 +0100

    hoi2

--- Engineering-ASPNET/Controllers/HomeController.cs
+++ Engineering-ASPNET/Controllers/HomeController.cs
@@ -24,11 +24,6 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult GebruikerAanmaak()
-    {
-        return View();
-    }
-
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
